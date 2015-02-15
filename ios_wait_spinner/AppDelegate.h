2342396e6950d6d5b47714ca8a87a3b71128740c
Maxim Bilan <maximb.mail@gmail.com>
2015-02-15 13:20:11 +0200

    Added project template

---
+++ ios_wait_spinner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ios_wait_spinner
+//
+//  Created by Maxim Bilan on 2/15/15.
+//  Copyright (c) 2015 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
