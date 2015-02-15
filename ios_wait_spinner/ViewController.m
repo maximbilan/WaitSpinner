a6b274808278c4032a769eab74e7c47bad9d1100
Maxim Bilan <maximb.mail@gmail.com>
2015-02-15 13:22:59 +0200

    Modified example

--- ios_wait_spinner/ViewController.m
+++ ios_wait_spinner/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "WaitSpinner.h"
 
 @interface ViewController ()
+{
+	WaitSpinner *waitSpinner;
+}
 
 @end
 
@@ -19,7 +23,15 @@
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 	
+	waitSpinner = [[WaitSpinner alloc] init];
 	
+	[waitSpinner showInView:self.view];
+	NSLog(@"Waiting... 3 seconds");
+	
+	dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+		[waitSpinner hide];
+		NSLog(@"Done");
+	});
 }
 
 - (void)didReceiveMemoryWarning
